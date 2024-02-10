54d362fe6168689a5a3122cfed72a148cfab1420
Dheepak Krishnamurthy <me@kdheepak.com>
2024-02-10 05:57:38 -0500

    wip: Remove search prompt for clarity

--- src/widgets.rs
+++ src/widgets.rs
@@ -3,7 +3,6 @@ pub mod help;
 pub mod popup_message;
 pub mod search_filter_prompt;
 pub mod search_page;
-pub mod search_prompt;
 pub mod search_results_table;
 pub mod status_bar;
 pub mod summary;
